cf61b56ee5d30e009c7ed6bdb4651eba178ec584
MR-PROFESSOR-790 <231360@students.au.edu.pk>
2025-08-08 15:55:03 +0500

    Update Uvicorn server port from 8000 to 8001

--- e-commerce/vector_service.py
+++ e-commerce/vector_service.py
@@ -60,4 +60,4 @@ def search(req: SearchRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8001)
