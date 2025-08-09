c3f2a22a2ca6a7245e03de2ce0b115fe1bb1cff6
MR-PROFESSOR-790 <231360@students.au.edu.pk>
2025-08-09 15:45:58 +0500

    feat: Implement greeting_agent with initial configuration and greeting logic

---
+++ Practice/1-basic-agent/greeting_agent/agent.py
@@ -0,0 +1,12 @@
+from google.adk.agents import LlmAgent
+from dotenv import load_dotenv
+
+load_dotenv()
+root_agent = LlmAgent(
+    name = "greeting_agent",
+    model = "gemini-2.0-flash",
+    description = "A simple agent that greets the user.",
+    instruction = "You are a friendly agent that greets the user warmly. Respond with a greeting message.",
+        
+)
+    
