58c3bf4d304f5ab12428c452cb2d22cafa4efa63
MR-PROFESSOR-790 <231360@students.au.edu.pk>
2025-08-09 17:03:28 +0500

    feat: Configure root_agent with model and search tool integration

--- Practice/2-tool_agent/tool_agent/agent.py
+++ Practice/2-tool_agent/tool_agent/agent.py
@@ -1,8 +1,13 @@
 from google.adk.agents.llm_agent import Agent
+from google.adk.tools import google_search
+from dotenv import load_dotenv
+
+load_dotenv()
 
 root_agent = Agent(
-    model='<FILL_IN_MODEL>',
+    model='gemini-2.0-flash',
     name='root_agent',
     description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
+    instruction='Answer user questions to the best of your knowledge. If you need to perform a search, use the google_search tool.',
+    tools=[google_search],
 )
