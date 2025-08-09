03db4e2e2ec29de0a783a8b920acba65e94261a2
MR-PROFESSOR-790 <231360@students.au.edu.pk>
2025-08-09 12:40:26 +0500

    Update dependencies for google-adk in pyproject.toml and uv.lock; change root_agent to LlmAgent in comparison agent

--- brand-SEO/sub_agent/comparison/agent.py
+++ brand-SEO/sub_agent/comparison/agent.py
@@ -2,10 +2,13 @@ from google.adk.agents import LoopAgent, LlmAgent
 from google.adk.sessions import InMemorySessionService
 from google.adk.runners import Runner
 from google.genai import types
+from .comparison.agent import comparison_root_agent
+from .sub_agent.search_result.agent import search_result_agent
+from .sub_agent.keyword_finding.agent import keyword_finding_agent
 from ...shared_libraries import constants
 from . import prompt
 
-root_agent = LoopAgent(
+root_agent = LlmAgent(
     model=constants.MODEL,
     name=constants.ROOT_AGENT_NAME,
     description=constants.ROOT_AGENT_DESCRIPTION,
