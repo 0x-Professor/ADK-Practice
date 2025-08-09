a706443c34cad387757986571701378f2d8172f4
MR-PROFESSOR-790 <231360@students.au.edu.pk>
2025-08-09 18:31:38 +0500

    feat: Update root_agent to use get_current_date tool and adjust instruction

--- Practice/2-tool_agent/tool_agent/agent.py
+++ Practice/2-tool_agent/tool_agent/agent.py
@@ -1,6 +1,12 @@
 from google.adk.agents.llm_agent import Agent
 from google.adk.tools import google_search
 from dotenv import load_dotenv
+import json
+import re
+import time
+from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
+import requests
+
 
 load_dotenv()
 def get_current_date() -> dict:
@@ -13,6 +19,8 @@ root_agent = Agent(
     model='gemini-2.0-flash',
     name='root_agent',
     description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge. If you need to perform a search, use the google_search tool.',
-    tools=[google_search],
+    instruction='Answer user questions to the best of your knowledge. If you need to perform a search, use the get_current_date tool.',
+    #tools=[google_search],
+    tools = [get_current_date],
+    
 )
