0298a14190d074682639ca445b2e219df33b1752
MR-PROFESSOR-790 <231360@students.au.edu.pk>
2025-08-10 11:57:51 +0500

    feat: Integrate LiteLLM model into dad_joke_agent for enhanced functionality

--- Practice/3-litellm_agent/create_joke/agent.py
+++ Practice/3-litellm_agent/create_joke/agent.py
@@ -1,8 +1,18 @@
 from google.adk.agents.llm_agent import Agent
+from google.adk.models.lite_llm import LiteLLM
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+model = LiteLLM(
+    model_name='openrouter/deepseek-chat-v3-0324:free',
+    api_key=os.getenv('OPENROUTER_API_KEY'),
+    #timeout=30,
+)
 
 root_agent = Agent(
-    model='<FILL_IN_MODEL>',
-    name='root_agent',
+    model=model,
+    name='dad_joke_agent',
     description='A helpful assistant for user questions.',
     instruction='Answer user questions to the best of your knowledge',
 )
